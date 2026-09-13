267ba35ec84183a6cf5d822e5b0e1c7003067bf6
Vincent Ly <vincent.ly0459@gmail.com>
2026-09-13 14:05:32 -0500

    Update main.py
    Added comments to main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 from oakd_device import OakDDevice
 from processors import YoloDrawer, SimpleSortTracker
 from outputs import FlaskServer, start_ros_publisher
-import threading
+import threading    # Used to run ROS2 in a background thread while the Flask server runs in the main thread.
 
 if __name__ == "__main__":
-    device = OakDDevice()
-    yolo_drawer = YoloDrawer()
-    tracker = SimpleSortTracker()
+    device = OakDDevice()           # Initialize the OAK-D device.
+    yolo_drawer = YoloDrawer()      # Initialize processors.
+    tracker = SimpleSortTracker()   # Initialize a simple tracking system.
 
+    # Prepare ROS2 publisher.
     ros_holder = {}
+    
+    # Create a background thread to run ROS2 publisher so that it doesn't block the Flask server.
     ros_thread = threading.Thread(target=start_ros_publisher,
                                   args=(ros_holder,),
                                   daemon=True)
+    
+    # Start the ROS2 publisher thread.
     ros_thread.start()
 
+    # Start the Flask server to serve the video stream and tracking data.
     server = FlaskServer(device, yolo_drawer, tracker, ros_holder)
     server.start()
